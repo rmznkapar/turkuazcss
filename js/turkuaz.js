8d0c46338df0d72f3fcf1a5e0c7f533f27d39533
rmznkapar <rmznkapar@gmail.com>
2017-09-06 22:35:28 +0300

    Update turkuaz.js

--- js/turkuaz.js
+++ js/turkuaz.js
@@ -9,6 +9,13 @@ Array.prototype.forEach.call(tag, function(el, i) {
     }, false);
 });
 
+var gra_sec = document.querySelectorAll('*[gra]');
+var gra_say;
+for(gra_say = 0; gra_say < gra_sec.length; gra_say++){
+    var gra_ata = gra_sec[gra_say].getAttribute("gra");
+    gra_sec[gra_say].style.background = "linear-gradient(to " +gra_ata+ ")";
+}
+
 $("select.tr-select").each(function() {
     var optionLength = $(this).children("option").length;
     $(this).addClass("gizle").wrap("<div class='select'></div>").after("<div class='tr-select-secili'></div>");
